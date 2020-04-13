b5107d2ee668dfdd6c74814fbf80c9828f76e0cf
C. Hans Culton <chansculton@gmail.com>
2020-04-13 22:25:47 +0930

    C# File Parsing complete

--- CSFileParser/CSFileParser/Program.cs
+++ CSFileParser/CSFileParser/Program.cs
@@ -1,32 +1,40 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace CSFileParser
 {
     class Program
     {
+        string tokeniser;
+        List<string> parsedData;
+
         static void Main(string[] args)
         {
-            //string filePath = Environment.CurrentDirectory;
-            //string filePAth = Path.Combine(Path.GetFullPath);
-            //string filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "//FileToParse.txt");
-            //string filePath = Path.GetFullPath("//FileToParse.txt");
-            //filePath += "//FileToParse.txt";
-            //string filePath = AppDomain.CurrentDomain.BaseDirectory + @"/FileTpParse.txt";
-            //string filePath = System.IO.Directory.GetFiles("../../FileTpParse.txt");
             string workingPath = Environment.CurrentDirectory;
             string projectPath = Directory.GetParent(workingPath).Parent.FullName;
             string filePath = Directory.GetParent(projectPath).Parent.FullName + "\\FileToParse.txt";
 
             Program currentProgram = new Program();
-            currentProgram.parseFile(filePath);
+            currentProgram.tokeniser = ":";
+            currentProgram.parsedData = new List<string>();
+            currentProgram.parseFile(filePath, currentProgram.parsedData);
 
-            Console.WriteLine(filePath);
+            for (int i = 0; i < currentProgram.parsedData.Count; i++)
+            {
+                Console.WriteLine(currentProgram.parsedData[i]);
+            }
         }
 
-        void parseFile(string _filePath)
+        void parseFile(string _filePath, List<string> _storage)
         {
             string[] fileLines = File.ReadAllLines(_filePath);
+
+            for (int i = 0; i < fileLines.Length; i++)
+            {
+                string[] tempArray = fileLines[i].Split(tokeniser);
+                _storage.Add(tempArray[1]);
+            }
         }
     }
 }
