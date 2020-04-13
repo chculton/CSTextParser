80c485f42cf02a40eea241616d1e221be589211a
C. Hans Culton <chansculton@gmail.com>
2020-04-13 22:13:39 +0930

    Programatically getting local directory path

--- CSFileParser/CSFileParser/Program.cs
+++ CSFileParser/CSFileParser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CSFileParser
 {
@@ -6,7 +7,26 @@ namespace CSFileParser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string filePath = Environment.CurrentDirectory;
+            //string filePAth = Path.Combine(Path.GetFullPath);
+            //string filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "//FileToParse.txt");
+            //string filePath = Path.GetFullPath("//FileToParse.txt");
+            //filePath += "//FileToParse.txt";
+            //string filePath = AppDomain.CurrentDomain.BaseDirectory + @"/FileTpParse.txt";
+            //string filePath = System.IO.Directory.GetFiles("../../FileTpParse.txt");
+            string workingPath = Environment.CurrentDirectory;
+            string projectPath = Directory.GetParent(workingPath).Parent.FullName;
+            string filePath = Directory.GetParent(projectPath).Parent.FullName + "\\FileToParse.txt";
+
+            Program currentProgram = new Program();
+            currentProgram.parseFile(filePath);
+
+            Console.WriteLine(filePath);
+        }
+
+        void parseFile(string _filePath)
+        {
+            string[] fileLines = File.ReadAllLines(_filePath);
         }
     }
 }
